4d2ae2f16a08e5af7fec14d159e8281727def144
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-10-15 14:10:50 -0600

    Update HomeController.cs

--- AspNetCoreWebApplication/Controllers/HomeController.cs
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -1,24 +1,82 @@
-﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using NationalParks.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetCoreWebApplication.Controllers
+namespace NationalParks.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
-            ViewData["Message"] = "You just created a ASP.Net Core web application!";
+            List<Park> parks = new List<Park>();
+            // Connect to MySql 
+            using (MySqlConnection con = new MySqlConnection("server=project1dbv2-instance-1.car4qphjnllt.us-east-2.rds.amazonaws.com; user=admin;database=Parks;port=3306;password=password123"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Parks", con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    //Extract your data
+                    Park park = new Park();
+                    park.Id = Convert.ToInt32(reader["Id"]);
+                    park.Name = reader["Name"].ToString();
+                    park.State = reader["State"].ToString();
+                    park.Date = Convert.ToInt32(reader["Date"]);
+                    park.Image = reader["Image"].ToString();
+
+                    parks.Add(park);
+                }
+                reader.Close();
+            }
+
+
+                return View(parks);
+        }
+
+        public IActionResult Privacy()
+        {
             return View();
         }
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            ViewData["Message"] = "We've encountered an error :(";
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //public IActionResult Index(IFormCollection form)
+        //{
+        //    string p = form["filterselect"];
+        //    IQueryable<> park = state.parks;
+        //    if (p == "California")
+        //    {
+        //        park = park.Where(x => x.california);
+        //    }
+        //    else if (p == "Utah")
+        //    {
+        //        park = park.Where(x => x.utah);
+        //    }
+            
+        //    ViewBag.ParkList = park;
+        //    ViewBag.Selected = p;
+        //    return View();
+        //}
+
     }
 }
