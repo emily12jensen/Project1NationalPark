6be054ab6d6bf5f97740873573e12e9b7c9c97f9
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-10-15 18:04:35 -0600

    Update Park.cs

--- AspNetCoreWebApplication/Models/Park.cs
+++ AspNetCoreWebApplication/Models/Park.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NationalParks.Models
+namespace AspNetCoreWebApplication.Models
 {
     public class Park
     {
